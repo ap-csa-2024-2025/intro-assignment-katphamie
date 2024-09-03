5d22d25c60b8120a6d5249011cdf3cbd9045fc73
katphamie <katnphmie@gmail.com>
2024-09-03 10:10:53 -0700

    hi!

---
+++ Printing.java
@@ -0,0 +1,17 @@
+public class Printing
+{
+    public static void main(String[] args)
+    {
+        // int, double, boolean, string
+        int myAge = 999; // int is for whole numbers; integers
+        double price = 4.99; //decimals
+
+        boolean isGood; //variable declaration
+        isGood = false; // variable intialization
+
+        String name = "Kathleen"; //set name to "Kathleen"
+
+        System.out.println("Hello my name is " + name);
+        System.out.print("I am " + myAge + " years old.");
+    }
+}
